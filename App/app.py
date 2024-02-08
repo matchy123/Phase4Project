3f5b59e34a94a852696532b8724499d8b17b1e7f
matchy123 <user.email>
2024-02-08 12:36:37 +0300

    initial

---
+++ App/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///animals.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
---
+++ App/models.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
